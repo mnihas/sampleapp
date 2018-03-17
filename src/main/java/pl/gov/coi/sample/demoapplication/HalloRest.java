4cce10487885683de2dbc0e5037e283dd70842d3
mnihas <michal.otroszczenko@gmail.com>
2018-03-18 00:32:52 +0100

    Wersja inicjalna

---
+++ src/main/java/pl/gov/coi/sample/demoapplication/HalloRest.java
@@ -0,0 +1,18 @@
+package pl.gov.coi.sample.demoapplication;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HalloRest {
+
+  @Value("${welcome.message}")
+  private String welcomeMessage;
+
+  @RequestMapping("/")
+  public String welcomeMessage() {
+    return welcomeMessage;
+  }
+
+}
---
+++ src/main/java/pl/gov/coi/sample/demoapplication/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.gov.coi.sample.demoapplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(DemoApplication.class);
+  }
+
+}
