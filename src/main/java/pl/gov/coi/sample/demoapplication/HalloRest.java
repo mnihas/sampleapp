9c4dc9a132fca7ef72a64c9aee318680f1a82f34
mnihas <michal.otroszczenko@gmail.com>
2018-03-18 22:36:27 +0100

    Dodanie MessageFormat

--- src/main/java/pl/gov/coi/sample/demoapplication/HalloRest.java
+++ src/main/java/pl/gov/coi/sample/demoapplication/HalloRest.java
@@ -3,6 +3,8 @@ package pl.gov.coi.sample.demoapplication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.MessageFormat;
+
 @RestController
 public class HalloRest {
 
@@ -13,7 +15,8 @@ public class HalloRest {
   @RequestMapping(value="/user/hello", method = RequestMethod.GET)
   public @ResponseBody
   String welcomeMessage(@RequestParam("name") String name) {
-    return String.format(welcomeMessage, name);
+      return MessageFormat.format(welcomeMessage, name);
+      //return String.format(welcomeMessage, name);
   }
 
 }
