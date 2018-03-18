eb1efdec26fbede7d658919f16a24785718a0ef9
mnihas <michal.otroszczenko@gmail.com>
2018-03-19 00:43:01 +0100

    Dodanie testów selenium

--- src/test/java/pl/gov/coi/sample/selenium/AppTest.java
+++ src/test/java/pl/gov/coi/sample/selenium/AppTest.java
@@ -9,14 +9,14 @@ public class AppTest {
     @Test
     public void firstTestMethod() {
         System.setProperty("webdriver.chrome.driver", "/opt/chromedriver/chromedriver");
-
+        //System.setProperty("webdriver.chrome.driver", "src/main/resources/selenium/chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--disable-extensions");
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.addArguments("--no-sandbox");
 
-        //WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         //driver.get("http://google.pl");
         System.out.println("Test passed");
@@ -24,3 +24,4 @@ public class AppTest {
         //driver.quit();
     }
 }
+
